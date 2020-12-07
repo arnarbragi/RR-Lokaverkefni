4f7e7b309abf64179f2c93ef707f97eec48e7f9e
arnarbragi <42640855+arnarbragi@users.noreply.github.com>
2020-12-07 23:20:49 +0000

    Skeleton

---
+++ RR-lokaverkefni.py
@@ -0,0 +1,36 @@
+#Diffra
+#Finna staðbundin útgildi
+#Finna útgildispunkta
+#Finna beygjuskil
+#Finna beygjuskilspunkt
+#Finna skurðpunkta við ásana
+
+class FallaRannsoknir:
+    def __init__(self, fall):
+        self.fall = fall
+
+    def diffra(self):
+        #Kóða hér
+
+    def utgildi(self):
+        #Kóða hér
+
+    def uPunktar(self):
+        #Kóða hér
+
+    def beygjuskil(self):
+        #Kóða hér
+
+    def bPunktur(self):
+        #Kóða hér
+
+    def skurdpkt(self):
+        #Kóða hér
+
+test = FallaRannsoknir("x2-2x")
+print(test.diffra())
+print(test.utgildi())
+print(test.uPunktar())
+print(test.beygjuskil())
+print(test.bPunktur())
+print(test.skurdpkt())
